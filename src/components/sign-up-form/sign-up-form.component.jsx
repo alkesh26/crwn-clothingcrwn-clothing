c7eb596fb4256b1de04fe8e410092becf644941d
Alkesh Ghorpade <alkesh26@gmail.com>
2025-09-09 00:56:57 +0530

    added styles for sign up component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -4,6 +4,7 @@ import {
   createUserDocumentFromAuth
 } from "../../utils/firebase/firebase.utils"
 import FormInput from "../form-input/form-input.component"
+import './sign-up-form.styles.scss'
 
 const defaultFormFields = {
   displayName: '',
@@ -48,8 +49,9 @@ const SignUpForm = () => {
   }
 
   return (
-    <div>
-      <h1>Sign Up with your email and password</h1>
+    <div className="sign-up-container">
+      <h2>I don't have an account</h2>
+      <span>Sign Up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label="Display Name"
