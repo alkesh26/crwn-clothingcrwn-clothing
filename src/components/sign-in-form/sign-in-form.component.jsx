0a26b7d57bb914da364f8f24d20a9524b80f0fc1
Alkesh Ghorpade <alkesh26@gmail.com>
2025-09-09 16:33:35 +0530

    Signout text change and use Context to set current user

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
 import {
   signInWithGooglePopup,
   signInAuthWithEmailAndPassword,
@@ -8,6 +8,8 @@ import FormInput from "../form-input/form-input.component"
 import './sign-in-form.styles.scss'
 import Button from "../button/button.component"
 
+import { UserContext } from "../../context/user.context"
+
 const defaultFormFields = {
   email: '',
   password: '',
@@ -16,6 +18,8 @@ const defaultFormFields = {
 const SignInForm = () => {
   const [formFields, setFormFields ] = useState(defaultFormFields)
   const { email, password } = formFields
+
+  const { setCurrentUser } = useContext(UserContext)
 
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
@@ -36,8 +40,8 @@ const SignInForm = () => {
     event.preventDefault()
 
     try {
-      const response = await signInAuthWithEmailAndPassword(email, password)
-      console.log(response)
+      const { user } = await signInAuthWithEmailAndPassword(email, password)
+      setCurrentUser(user)
       resetFormFields()
     } catch(error) {
       switch(error.code) {
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,12 @@
-import { Fragment } from 'react'
+import { Fragment, useContext } from 'react'
 import { Outlet, Link } from 'react-router-dom'
 import './navigation.styles.scss'
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+import { UserContext } from '../../context/user.context'
 
 export default function Navigation() {
+  const { currentUser } = useContext(UserContext)
   return (
     <Fragment>
       <div className='navigation'>
@@ -18,9 +20,15 @@ export default function Navigation() {
           <Link className='nav-link' to="/contact">
             CONTACT
           </Link>
-          <Link className='nav-link' to="/auth">
-            SIGN IN
-          </Link>
+          {
+            currentUser ? (
+              <span className='nav-link'>SIGN OUT</span>
+            ) : (
+              <Link className='nav-link' to="/auth">
+                SIGN IN
+              </Link>
+            )
+          }
         </div>
       </div>
       <Outlet />
