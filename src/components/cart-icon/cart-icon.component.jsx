78a7039726a7a7eede17dbe01f757998ce235371
Alkesh Ghorpade <alkesh26@gmail.com>
2025-09-11 14:07:25 +0530

    Cart dropdown toggle

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,10 +1,18 @@
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
+import { CartContext } from '../../context/cart.context.jsx'
+import { useContext } from 'react'
 
 import './cart-icon.styles.scss'
 
 export default function CartIcon() {
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext)
+
+  function toggleIsCartOpen() {
+    setIsCartOpen(!isCartOpen)
+  }
+
   return(
-    <div className='cart-icon-container'>
+    <div className='cart-icon-container' onClick={toggleIsCartOpen}>
       <ShoppingIcon className='shopping-icon'/>
       <span className='item-count'>0</span>
     </div>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -5,11 +5,13 @@ import './navigation.styles.scss'
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
 import { UserContext } from '../../context/user.context'
 import { signOutUser } from '../../utils/firebase/firebase.utils'
+import { CartContext } from '../../context/cart.context'
 import CartIcon from '../../components/cart-icon/cart-icon.component'
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 
 export default function Navigation() {
   const { currentUser } = useContext(UserContext)
+  const { isCartOpen } = useContext(CartContext)
 
   return (
     <Fragment>
@@ -33,11 +35,9 @@ export default function Navigation() {
               </Link>
             )
           }
-          <Link className='nav-link'>
-            <CartIcon />
-          </Link>
+          <CartIcon />
         </div>
-        <CartDropdown />
+        { isCartOpen && <CartDropdown /> }
       </div>
       <Outlet />
     </Fragment>
