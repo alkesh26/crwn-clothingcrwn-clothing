3dbffb12670b5775bff19071baf50598e94e3f24
Alkesh Ghorpade <alkesh26@gmail.com>
2025-09-14 17:34:58 +0530

    Added category navigation from category preview page

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from 'react'
+import { useContext, useState, useEffect, Fragment } from 'react'
 import { useParams } from 'react-router-dom'
 import { CategoriesContext } from '../../context/categories.context'
 import './category.styles.scss'
@@ -14,11 +14,14 @@ export default function Category() {
   }, [category, categoriesMap])
 
   return (
-    <div className='category-container'>
-      {
-        products &&
-          products.map((product) => <ProductCard key={product.id} product={product} />)
-      }
-    </div>
+    <Fragment>
+      <h2 className='category-title'>{category.toUpperCase()}</h2>
+      <div className='category-container'>
+        {
+          products &&
+            products.map((product) => <ProductCard key={product.id} product={product} />)
+        }
+      </div>
+    </Fragment>
   )
 }
