43e10f569cc93d074739bda82d1a6bdcd39cc94a
Alkesh Ghorpade <alkesh26@gmail.com>
2025-09-14 16:26:36 +0530

    Moved the shop page to category preview component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,5 +1,6 @@
 import { useContext, Fragment } from "react"
 import { CategoriesContext } from '../../context/categories.context.jsx'
+import CategoryPreview from "../../components/category-preview/category-preview.component.jsx"
 import ProductCard from '../../components/product-card/product-card.component.jsx'
 import './shop.styles.scss'
 
@@ -7,22 +8,15 @@ const Shop = () => {
   const {categoriesMap} = useContext(CategoriesContext)
 
   return (
-    <Fragment>
+    <div className="shop-container">
       {
-        Object.keys(categoriesMap).map(title => (
-          <Fragment key={title}>
-            <h2>{title}</h2>
-            <div className="products-container">
-              {
-                categoriesMap[title].map((product) =>
-                  (<ProductCard key={product.id} product={product} />)
-                )
-              }
-            </div>
-          </Fragment>
-        ))
+        Object.keys(categoriesMap).map(title => {
+          const products = categoriesMap[title]
+
+          return <CategoryPreview key={title} title={title} products={products} />
+        })
       }
-    </Fragment>
+    </div>
   )
 }
 
