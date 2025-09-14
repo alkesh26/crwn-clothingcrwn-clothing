54ed8a612e6ddd3164860503f1ddc7b490f82bd3
Alkesh Ghorpade <alkesh26@gmail.com>
2025-09-14 17:01:59 +0530

    Category page changges

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,24 @@
+import { useContext, useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { CategoriesContext } from '../../context/categories.context'
+import './category.styles.scss'
+import ProductCard from '../../components/product-card/product-card.component'
+
+export default function Category() {
+  const { categoriesMap } = useContext(CategoriesContext)
+  const { category } = useParams()
+  const [products, setProducts] = useState(categoriesMap[category])
+
+  useEffect(() => {
+    setProducts(categoriesMap[category])
+  }, [category, categoriesMap])
+
+  return (
+    <div className='category-container'>
+      {
+        products &&
+          products.map((product) => <ProductCard key={product.id} product={product} />)
+      }
+    </div>
+  )
+}
